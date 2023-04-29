15a775086b5d611f0e72d51fb9153fc5adf44e4d
Pshibiev <dengi.eu@yandex.ru>
2023-04-29 14:56:18 +0300

    first commit

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,6 @@
+export function AboutPage() {
+    return(
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni tempore molestiae sint quos eligendi neque, sed officiis voluptatibus! Error unde omnis quasi consequuntur recusandae vitae. Blanditiis sed, doloremque vero quidem provident distinctio, laboriosam at ipsa corporis qui magnam cupiditate odit illo libero amet voluptate aliquid nesciunt culpa harum rem ex!            
+        </div>
+    )
+}
